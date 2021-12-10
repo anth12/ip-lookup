a646415ad762e2155c7b5f80271fc07f114bc8fd
Anthony Halliday <anthony.halliday@fatmedia.co.uk>
2021-12-10 09:06:41 +0000

    Added domain contracts

---
+++ src/IpLookup.Domain/GeoCoordinate.cs
@@ -0,0 +1,15 @@
+﻿
+namespace IpLookup.Domain
+{
+    public struct GeoCoordinate
+    {
+        public GeoCoordinate(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public double Latitude { get; }
+        public double Longitude { get; }
+    }
+}
---
+++ src/IpLookup.Domain/Location.cs
@@ -0,0 +1,23 @@
+﻿
+namespace IpLookup.Domain
+{
+    public class Location
+    {
+        public Location(string city, string country, string continent, GeoCoordinate coordinate, decimal timezoneOffset)
+        {
+            City = city;
+            Country = country;
+            Continent = continent;
+            Coordinate = coordinate;
+            TimezoneOffset = timezoneOffset;
+        }
+
+        public string City { get; }
+        public string Country { get; }
+        public string Continent { get; }
+
+        public GeoCoordinate Coordinate { get; }
+
+        public decimal TimezoneOffset { get; }
+    }
+}
